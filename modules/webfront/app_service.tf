58e0a74524c59c74f1b2dacb5d93fc371c0ed477
Guy Diangana <gdiangana@gmail.com>
2026-09-12 19:44:10 +0000

    Restrict App Service access to WAF subnet

--- modules/webfront/app_service.tf
+++ modules/webfront/app_service.tf
@@ -8,15 +8,25 @@ resource "azurerm_service_plan" "web" {
 }
 
 resource "azurerm_linux_web_app" "web" {
-  name                = "northstar-lz-web-244d"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  service_plan_id     = azurerm_service_plan.web.id
-  https_only          = true
-  tags                = var.tags
+  name                                           = "northstar-lz-web-244d"
+  resource_group_name                            = var.resource_group_name
+  location                                       = var.location
+  service_plan_id                                = azurerm_service_plan.web.id
+  https_only                                     = true
+  ftp_publish_basic_authentication_enabled       = false
+  webdeploy_publish_basic_authentication_enabled = false
+  tags                                           = var.tags
 
   site_config {
-    always_on = true
+    always_on                     = true
+    ip_restriction_default_action = "Deny"
+
+    ip_restriction {
+      name                      = "Allow-WAF-Subnet"
+      priority                  = 100
+      action                    = "Allow"
+      virtual_network_subnet_id = var.waf_subnet_id
+    }
 
     application_stack {
       docker_image_name   = "nginx"
