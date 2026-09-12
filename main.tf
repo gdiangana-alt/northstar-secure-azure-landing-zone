0bbe24014d0f56f17f9088b9cedcedf902bf7ab9
Guy Diangana <gdiangana@gmail.com>
2026-09-12 04:58:46 +0000

    Manage Sentinel monitoring and detection with Terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+data "azurerm_subscription" "current" {}
+
 resource "azurerm_resource_group" "landing_zone" {
   name     = var.resource_group_name
   location = var.location
@@ -25,4 +27,12 @@ module "identity" {
   resource_group_id   = azurerm_resource_group.landing_zone.id
   location            = azurerm_resource_group.landing_zone.location
   tags                = var.tags
+}
+
+module "monitoring" {
+  source = "./modules/monitoring"
+
+  workspace_name                = "NorthStar-SOC-Workspace"
+  workspace_resource_group_name = "NorthStar-Azure-RG"
+  subscription_id               = data.azurerm_subscription.current.subscription_id
 }
